d462c82ee7b6cfeceecf81f37357f2a675cd9aa3
chan9yu <wry5533@gmail.com>
2024-01-23 20:42:43 +0900

    chore: eslint, tsconfig 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'no-unused-vars': 'warn'
 	}
 };
