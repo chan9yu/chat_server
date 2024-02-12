0fc3757675b828f75faaa18464e526d33f8c3305
chan9yu <wry5533@gmail.com>
2024-02-12 16:26:38 +0900

    feat: mvc 패턴 제거

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,12 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-	root() {
-		return {
-			data: {
-				title: 'Simple Meet',
-				copyright: 'chan9yu'
-			}
-		};
+	getHello(): string {
+		return `Hello, we're simple meet server`;
 	}
 }
